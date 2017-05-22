8ce5edd569c61b030614bb3617e7381ffe1abc6d
Rick <ricardolira48@hotmail.com>
2017-05-22 12:00:04 -0400

    22/5 12:00

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,45 +36,98 @@ function remove(element){
     element.parentNode.parentNode.removeChild(element.parentNode);
 }
 
+var current = mercadeo
 
+var mercadeo = [["Direccion de Ingreso, Consumo Fidel y Reten", "Gerencia de Retención y Fidelización","Gerencia General de Maximización de Ingresos y Consumo - Ingresos Masivos",
+                  "Gerencia General de Maximización de Ingresos y Consumo - Consumo","Gerencia Gestión de Ingresos"],
+                ["Gerencia General Planificación y Administración"],
+                ["Gerencia General Inteligencia de Negocios", "Gerencia Ingeniería del Dato",
+                  "Gerencia Conocimiento del cliente"],
+                ["Gerencia Experiencia del Cliente"],
+                ["Gerencia General Movistar TV","Gerencia Comercial Movistar TV","Gerencia Atención multicanal Movistar TV"],
+                ["Gerencia General Captación","Gerencia de Certificación y operaciones","Gerencia de Oferta comercial"],
+                ["Gerencia General de Publicidad y medios","Gerencia de Publicidad","Gerencia de Eventos","Gerencia de Patrocinios","Gerencia de Medios"]]
 
-var mercadeo = ["Direccion de Ingreso, Consumo Fidel y Reten","Gerencia General Planificación y Administración",
-                "Gerencia General Inteligencia de Negocios","Gerencia Experiencia del Cliente","Gerencia General Movistar TV",
-                "Gerencia General Captación","Gerencia General de Publicidad y medios"]
+var perc = [["Gerencia General Planificación, Org. Y Compens.","1","2","3"],["Gerencia General Desarrollo, Organiz y Talento","2","3","4"],
+            ["Gerencia General Experiencia al Empleado","3","4","5"]]    
 
-var rrhh = ["Gerencia General Planificación, Org. Y Compens.","Gerencia General Desarrollo, Organiz y Talento",
-            "Gerencia General Experiencia al Empleado"]                
+var rrhh = [["Gerencia General Planificación, Org. Y Compens.","1","2","3"],["Gerencia General Desarrollo, Organiz y Talento","2","3","4"],
+            ["Gerencia General Experiencia al Empleado","3","4","5"]] 
+
+var ggft = [["Gerencia General Planificación, Org. Y Compens.","1","2","3"],["Gerencia General Desarrollo, Organiz y Talento","2","3","4"],
+            ["Gerencia General Experiencia al Empleado","3","4","5"]] 
+
+var tech = [["Gerencia General Planificación, Org. Y Compens.","1","2","3"],["Gerencia General Desarrollo, Organiz y Talento","2","3","4"],
+            ["Gerencia General Experiencia al Empleado","3","4","5"]] 
+
+var sc = [["Gerencia General Planificación, Org. Y Compens.","1","2","3"],["Gerencia General Desarrollo, Organiz y Talento","2","3","4"],
+            ["Gerencia General Experiencia al Empleado","3","4","5"]] 
+
+var drig = [["Gerencia General Planificación, Org. Y Compens.","1","2","3"],["Gerencia General Desarrollo, Organiz y Talento","2","3","4"],
+            ["Gerencia General Experiencia al Empleado","3","4","5"]]    
+
+var dsf = [["Gerencia General Planificación, Org. Y Compens.","1","2","3"],["Gerencia General Desarrollo, Organiz y Talento","2","3","4"],
+            ["Gerencia General Experiencia al Empleado","3","4","5"]] 
+
+var da = [["Gerencia General Planificación, Org. Y Compens.","1","2","3"],["Gerencia General Desarrollo, Organiz y Talento","2","3","4"],
+            ["Gerencia General Experiencia al Empleado","3","4","5"]] 
+
+var emp = [["Gerencia General Planificación, Org. Y Compens.","1","2","3"],["Gerencia General Desarrollo, Organiz y Talento","2","3","4"],
+            ["Gerencia General Experiencia al Empleado","3","4","5"]] 
+
+var fcg = [["Gerencia General Planificación, Org. Y Compens.","1","2","3"],["Gerencia General Desarrollo, Organiz y Talento","2","3","4"],
+            ["Gerencia General Experiencia al Empleado","3","4","5"]] 
+
+var canales = [["Gerencia General Planificación, Org. Y Compens.","1","2","3"],["Gerencia General Desarrollo, Organiz y Talento","2","3","4"],
+            ["Gerencia General Experiencia al Empleado","3","4","5"]] 
+          
 
 function alert(element){
     if (element.value == "VP MERCADEO"){
         $("#ggs").empty();
+        var blank = document.createElement("option");
+            blank.text = " ";
+            blank.value = " ";
+            $("#ggs").append(blank);
+        current = mercadeo
         for( var i = 0; i < mercadeo.length ; i++){
 
             var option = document.createElement("option");
-            option.text = mercadeo[i];
-            option.value = mercadeo[i];
+            option.text = mercadeo[i][0];
+            option.value = mercadeo[i][0];
             $("#ggs").append(option);
         }
     }else{
         $("#ggs").empty();
+        var blank = document.createElement("option");
+            blank.text = " ";
+            blank.value = " ";
+            $("#ggs").append(blank);
+        current = rrhh
         for( var i = 0; i < rrhh.length ; i++){
             var option = document.createElement("option");
-            option.text = rrhh[i];
-            option.value = rrhh[i];
+            option.text = rrhh[i][0];
+            option.value = rrhh[i][0];
             $("#ggs").append(option);
         }
     } 
-
-/*
-    $("#areas")[0].disabled = false; 
-    $("#areas").empty();
-    var option2 = document.createElement("option");
-    option2.text = " ";
-    option2.value = " ";
-    $("#areas")[0].append(option2)
-    var option = document.createElement("option");
-    option.text = element.value;
-    option.value = element.value;
-    $("#areas")[0].append(option)*/
     
+}
+
+function alert2(element){
+    gg = []
+    for(var i=0;i < current.length;i++){
+        if (element.value == current[i][0]){
+            gg = current[i];
+            i = current.length;
+        }
+    }
+    $("#gerencias").empty();
+        for( var i = 1; i < gg.length ; i++){
+            var option = document.createElement("option");
+            option.text = gg[i];
+            option.value = gg[i];
+            $("#gerencias").append(option);
+        }
+
 }
